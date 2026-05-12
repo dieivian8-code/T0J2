8a88ca440422a2a213ef9a4cea22162e211de9da
Dieivian Morôni <dieivian8@gmail.com>
2026-05-12 15:51:45 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,33 @@
+console.log('Hello Harianna')
+// tipos de variaveis
+// const-valor não é alteravel] ex -cpf
+const nome='camaro carlos'
+// let- pode ser alterado- idade ou endereço
+let idade='16'
+// var forma antiga de declarar uma variavel-NÃO DEVE SER UTILIZADO
+var endereço='lindolfo collor 69'
+
+// tipos de dados
+
+
+// string variavel do tipo texto
+console.log('Hello this our class of JS');
+console.log('hello instuticon percorre');
+console.log('now yes i can broke the use or more lines');
+
+// Number - tipo numerico
+
+let numerointeiro=30
+let numeronegativo=-87
+let numerodecimal=-5.67
+
+numerodecimal=8.97
+console.log(numerodecimal)
+
+// Boolean- tipo dado booleano  teste se for verdadeiro ou falso
+
+console.log(25>78);
+console.log(23 >- 23);
+
+
+
---
+++ variaveis2.js
@@ -0,0 +1,12 @@
+console.log(' hello moronaine')
+
+const nome='dieivian'
+
+let  idade='16'
+
+
+
+console.log('surely will be working')
+console.log('do you havent try to yourself?')
+console.log('no exacly knowing eachother its is better')
+
