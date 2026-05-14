4036643ac8680f71554062887b1e29f0cf658171
Dieivian Morôni <dieivian8@gmail.com>
2026-05-14 15:55:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// let var const
+let n1,n2,media,nome
+
+
+n1 = 8
+n2 = 7
+media = 8
+nome = 'tonia'
+//  |''
+console.log('a nota da ' + nome + ' é : ' + media);
+console.log('a nota de ${nome} é :${media}');
---
+++ operadora.js
@@ -0,0 +1,57 @@
+// let nome = 'luana'
+// let n1 = 8
+// let n2 = 10
+// let n3 = 4
+
+// let media = ( n1 + n2 + n3 ) / 3 
+
+// console.log(`a media da ${nome} é :${media}`);
+// let n1 = 10
+// let n2 = 7
+// let n3 = 8
+// let n4 =  3
+
+// let media, nome;
+// nome = "lauane"
+// media = (n1 + n2 + n3 + n4)/4
+// console.log(`A aluna: ${nome}`);
+// console.log(`Ficou com média: ${media}`);
+   // > maior
+// >= maior ou igual
+// < menor
+// <= menor ou igual
+// let media = 7
+// console.log(media <= 7);
+
+// 7 == 7 = true
+// 7 === "7" = false
+// 7 != 8 = true
+// 
+
+// 
+
+
+
+
+
+
+
+
+
+
+// agora é com você!
+
+// crie três variaveis: media, faltas e nome
+// faça um teste com  o operador ternario para testar se o  aluno será aprovado.
+// regras: média deve ser maior que 7 e faltas menor que 5 passe a resposta no console           
+let = 8
+let = 10
+let = 4
+let = 7
+
+
+let falta
+let media = (n1 + n2 + n3 + n4)/4
+let nome = 'Morôni'
+console.log(`A `)
+console.log(``)
